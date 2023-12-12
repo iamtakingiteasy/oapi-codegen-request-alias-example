3b72472f8c2b9dfe62eab4579ba372434814d0c7
Alexander Tumin <iamtakingiteasy@eientei.org>
2023-12-12 22:40:32 +0300

    initial commit

---
+++ api.go
@@ -0,0 +1,3 @@
+//go:generate $GOPATH/bin/oapi-codegen -config codegen_components.yaml openapi_components.yaml
+//go:generate $GOPATH/bin/oapi-codegen -config codegen_service.yaml openapi_service.yaml
+package testmodule
